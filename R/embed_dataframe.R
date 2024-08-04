aa6fd0bc1047618f907c472fc997f86c2580264b
drgomulka <80592261+drgomulka@users.noreply.github.com>
2024-08-05 00:36:27 +0200

    Create embed_dataframe.R

---
+++ R/embed_dataframe.R
@@ -0,0 +1,40 @@
+embed_dataframe <- function(df, dimension = 1) {
+  if (!is.data.frame(df)) {
+    stop("'df' must be a data frame")
+  }
+  if ((dimension < 1) || (dimension >  nrow(df))) {
+    stop("Invalid embedding dimension")
+  }
+
+  n_rows <- nrow(df)
+  n_cols <- ncol(df)
+  
+  new_colnames <- colnames(df)
+  for (i in 1:(dimension-1)) {
+    temp <- paste(colnames(df), ".l", i, sep = "")
+    new_colnames <- c(new_colnames, temp)
+  }
+  
+  new_number_of_rows <- n_rows - dimension + 1L
+  new_number_of_cols <- dimension * n_cols
+
+  df_out <- data.frame(matrix(0, new_number_of_rows, new_number_of_cols))
+
+  for (i in seq_len(m)) {
+    source_column_i <- as.vector(df[, i])
+    
+    for (j in  seq_len(dimension)-1 ) {    
+      col_number <- i + n_cols*j
+      range_of_rows <- (dimension-j):(n_rows-j) 
+      shortened_column <- source_column_i[range_of_rows]
+      df_out[, col_number] <- shortened_column
+    }
+  }
+  
+  colnames(df_out) <- new_colnames
+  return(df_out)
+}
+
+#dff <-  data.frame(x = 1:10,y = 11:20,  z = 21:30)
+#dff
+#embed_dataframe(dff, dimension = 4)
