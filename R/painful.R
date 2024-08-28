a5cf74577591db3feef3c2edfb6583bf93730db0
drgomulka <80592261+drgomulka@users.noreply.github.com>
2024-08-28 14:41:08 +0200

    Update painful.R

--- R/painful.R
+++ R/painful.R
@@ -2,10 +2,10 @@ df_a <- data.frame(a=1:5)
 df_ab <- data.frame(a=1:5, b=5:1)
 dput(df_a)
 dput(df_ab)
-df_a[c(1, 2),]
-df_ab[c(1, 2),]
-dput( df_a[c(1, 2),] )
-dput (df_ab[c(1, 2),] )
+df_a[1,]
+df_ab[1,]
+dput( df_a[1,] )
+dput (df_ab[1,] )
 
 # look what happens:
 > df_a <- data.frame(a=1:5)
@@ -16,14 +16,12 @@ structure(list(a = 1:5), class = "data.frame", row.names = c(NA,
 > dput(df_ab)
 structure(list(a = 1:5, b = 5:1), class = "data.frame", row.names = c(NA, 
 -5L))
-> df_a[c(1, 2),]
-[1] 1 2
-> df_ab[c(1, 2),]
+> df_a[1,]
+[1] 1
+> df_ab[1,]
   a b
 1 1 5
-2 2 4
-> dput( df_a[c(1, 2),] )
-1:2
-> dput (df_ab[c(1, 2),] )
-structure(list(a = 1:2, b = 5:4), row.names = 1:2, class = "data.frame")
-> 
+> dput( df_a[1,] )
+1L
+> dput (df_ab[1,] )
+structure(list(a = 1L, b = 5L), row.names = 1L, class = "data.frame")
