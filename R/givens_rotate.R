c23e0a73653b347fd64540a02eb0e70ddcaf7601
drgomulka <80592261+drgomulka@users.noreply.github.com>
2025-09-04 14:44:52 +0200

    Update givens_rotate.R

--- R/givens_rotate.R
+++ R/givens_rotate.R
@@ -1,11 +1,14 @@
 
-givens_by_row <- function(X, j) {
-
+givens_single_rotation <- function(X, idcol) {
+  stopifnot(is.matrix(X))
+  stopifnot("idcol must be not larger than the number of columns of X" = ncol(X) >= idcol)
   stopifnot(nrow(X) ==2)
-  c <- X[1,j]
-  s <- X[2,j]
-d  <- hypot(c,s) 
-Grotation <-  matrix( c(c, -s, s, c) , 2,2 )/d
-Grotation  %*% X 
- 
+  
+  c <- X[1,idcol]
+  s <- X[2,idcol]
+  d  <- sqrt( c^2 + s^2 )
+  Grotation <-  matrix( c(c, -s, s, c) , 2,2 )/d
+  X_returned <- Grotation  %*% X 
+  #X_returned[2,idcol] <- 0
+  X_returned
 }
