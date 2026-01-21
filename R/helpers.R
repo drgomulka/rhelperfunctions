d5d3ea5f30d49c51f3464e3781f18f87738379f4
drgomulka <80592261+drgomulka@users.noreply.github.com>
2026-01-21 14:45:34 +0100

    Implement repair_fable function for fable objects
    Added repair_fable function to restore attributes in fable objects.

--- R/helpers.R
+++ R/helpers.R
@@ -1,2 +1,20 @@
 
 reduce_by_sum <- function(x)  {  purrr::reduce (x , `+`)  }
+
+
+
+#' Title repair_fable() repairs tables which lost attributes
+#'
+#' @param df  fable object to repair
+#' @param varname  character string correct value
+#'
+#' @returns repaired table
+#'
+#' @examples  repair_fable(df_1, "somevariablename")
+repair_fable <- function(df, varname) {
+  # require(fabletools)
+  temp <- df[[varname]]
+  dimnames(temp) <- varname
+  df[[varname]]  <- temp
+  fabletools::as_fable(df, response = !!varname, distribution = !!varname)
+}
